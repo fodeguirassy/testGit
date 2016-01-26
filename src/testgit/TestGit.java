b4e27c616bfb30947940e0d11930b978d720217f
fodeguirassy <fodeguirassy@172.17.16.9>
2016-01-26 14:30:25 +0100

    Test effacer commentaires

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -1,16 +1,13 @@
 
 package testgit;
 
-/**
- *
- * @author fodeguirassy
- */
 public class TestGit {
 
     public static void main(String[] args) {
         // TODO code application logic here
         String test= "";
         System.out.println(test);
+        
     }
     
 }
