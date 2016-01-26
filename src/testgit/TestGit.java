e0d551902c1995ce0bcb12059d1bfa7e6b84d880
fodeguirassy <fodeguirassy@172.17.16.9>
2016-01-26 14:23:22 +0100

    first test Git

---
+++ src/testgit/TestGit.java
@@ -0,0 +1,17 @@
+
+package testgit;
+
+/**
+ *
+ * @author fodeguirassy
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
